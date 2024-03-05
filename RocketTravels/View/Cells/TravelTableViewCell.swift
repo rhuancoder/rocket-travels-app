d3d6b46325846fa2b462ec9a3690226176b1c7cb
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-03-04 22:09:49 -0300

    feat: add travel cell

---
+++ RocketTravels/View/Cells/TravelTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelTableViewCell.swift
+//  RocketTravels
+//
+//  Created by Rhuan Carvalho on 04/03/24.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
