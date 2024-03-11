598d6d61d88c2f1cc84756561098594a153b2ea9
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-03-11 20:03:59 -0300

    feat: change table size

--- RocketTravels/View/ViewController.swift
+++ RocketTravels/View/ViewController.swift
@@ -61,6 +61,6 @@ extension ViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 400
+        return UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone ? 400 : 480
     }
 }
