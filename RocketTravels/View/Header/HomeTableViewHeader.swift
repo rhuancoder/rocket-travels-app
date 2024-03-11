71aaa842c64ae303f3b038e3ce8f4d4f57f99358
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-03-11 19:49:32 -0300

    feat: change header size

--- RocketTravels/View/Header/HomeTableViewHeader.swift
+++ RocketTravels/View/Header/HomeTableViewHeader.swift
@@ -16,7 +16,7 @@ class HomeTableViewHeader: UIView {
     
     func setupView() {
         headerView.backgroundColor = UIColor(red: 30/255, green: 59/255, blue: 119/255, alpha: 1)
-        headerView.layer.cornerRadius = 500
+        headerView.layer.cornerRadius = UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone ? 500 : 200
         headerView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         
         bannerView.layer.cornerRadius = 10
